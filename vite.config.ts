6099e5e0eb1ed9aa9bd5462d1fde2bbe3185e774
Hadil <bensoussiahadil2003@gmail.com>
2025-12-05 19:39:02 +0100

    Correction du chemin de base (vite.config.ts) pour deploiement Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // AJOUTEZ CETTE LIGNE : Spécifie le chemin de base pour le déploiement sur GitHub Pages
+  base: '/portfolio-hadil/',
   plugins: [react()],
 })
