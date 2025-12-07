4cc91a11d8ade1da8899b2f077dd943a54111b8a
Hadil <bensoussiahadil2003@gmail.com>
2025-12-07 19:29:40 +0100

    potfolio finale

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,67 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
+  const location = useLocation();
+  
+  const isProjectPage = () => {
+    const currentPath = location.pathname;
+    return (
+      currentPath === "/projects" ||
+      currentPath === "/cap-projet" ||
+      currentPath === "/esieabot"
+      
+    );
+  };
+
   return (
     <nav className="navbar">
       <h1 className="logo">Portfolio — Hadil BEN SOUSSIA</h1>
 
       <div className="nav-links">
         
-        <NavLink to="/" className="nav-item">
+        <NavLink 
+          to="/" 
+          className={({ isActive }) => 
+            isActive ? "nav-item active" : "nav-item"
+          }
+        >
           Accueil
         </NavLink>
 
-        <NavLink to="/projects" className="nav-item">
+        <NavLink 
+          to="/projects" 
+          className={
+            isProjectPage() ? "nav-item active" : "nav-item"
+          }
+        >
           Projets
         </NavLink>
 
-        <NavLink to="/skills" className="nav-item">
+        <NavLink 
+          to="/skills" 
+          className={({ isActive }) => 
+            isActive ? "nav-item active" : "nav-item"
+          }
+        >
           Compétences
         </NavLink>
 
-        <NavLink to="/cv" className="nav-item">
+        <NavLink 
+          to="/cv" 
+          className={({ isActive }) => 
+            isActive ? "nav-item active" : "nav-item"
+          }
+        >
           CV
         </NavLink>
 
-        <NavLink to="/contact" className="nav-item">
+        <NavLink 
+          to="/contact" 
+          className={({ isActive }) => 
+            isActive ? "nav-item active" : "nav-item"
+          }
+        >
           Contact
         </NavLink>
 
